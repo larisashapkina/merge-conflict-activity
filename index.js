bde9aa5f48904f79e0eadf6e3672c4b2f9424a2c
Sara D'Arce <saradarce@gmail.com>
2021-12-02 16:54:00 -0500

    sara-strings

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
-      throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
-  }
-  
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  if (!c.every((a) => Number.isNumber(a)))
+    throw "All arguments must be numbers.";
+  return c.reduce((a, b) => a + b);
+}
+
+fn(1, 2, 3); //> 6
+fn(10, "B", 20, "giraffe", "bubble"); //> error All arguments must be numbers.
